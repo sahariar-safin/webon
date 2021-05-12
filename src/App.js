dd320523de048d773f06d0d599a0b959c175ee4b
Shafin The Developer <sahariar350@gmail.com>
2021-05-12 14:35:47 +0600

    something is cooking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Home from './componant/Home/Home/Home';
 
 function App() {
--- src/componant/Home/Navbar/Navbar.js
+++ src/componant/Home/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Navbar.scss';
+import logo from '../../../images/logo-light-img-1-1.png'
 
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <img className='logo' src={logo} alt="" />
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -19,7 +21,8 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="me-auto">
-                    <button>Search</button>
+                    <button className='nav-btn'>Search</button>
+                    <button className='nav-btn'>Search</button>
                 </div>
             </div>
         </nav>
